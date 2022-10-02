45823fd503a21cdd702b086754f6f2c0f9ef9917
Jaydeep <jaydeep.dey03@gmail.com>
2022-10-02 23:09:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './pages/Home';
+// eslint-disable-next-line
+import "swiper/css/bundle";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
